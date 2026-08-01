79cc5d285b86bef6f8e85e5c16ebb6ee28b3aff1
Cloud305 <161991469+cloudaws305@users.noreply.github.com>
2026-08-01 19:13:20 +0530

    Update auto_fix.py

--- auto_fix.py
+++ auto_fix.py
@@ -4,7 +4,7 @@ from git import Repo
 from github import Github
 
 REPO_NAME = "cloudaws305/test"
-BRANCH = "ai-auto-fix_5"
+BRANCH = "ai-auto-fix_6"
 FILE = "app.py"
 
 # Read file
@@ -35,7 +35,7 @@ print("Verification passed.")
 
 # Git operations
 
-token = "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"
+token = "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"
 
 github = Github(token)
 
